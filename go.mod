c06ad4ad29f10620f5c9b18b2eb31a38001f2b72
11038569+calamity-m@users.noreply.github.com <11038569+calamity-m@users.noreply.github.com>
2024-12-02 23:10:53 +1100

    Initial commit of structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/calamity-m/reap
+
+go 1.23.3
---
+++ services/sow/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/calamity-m/reap/shared/go/auth"
+)
+
+func main() {
+	fmt.Println("wtf")
+
+	auth.Auth()
+
+}
