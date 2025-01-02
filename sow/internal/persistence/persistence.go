df98e23bf97383856593e620ea033b1b913bfa0b
11038569+calamity-m@users.noreply.github.com <11038569+calamity-m@users.noreply.github.com>
2025-01-02 11:45:02 +1100

    basic mem store

--- sow/internal/persistence/persistence.go
+++ sow/internal/persistence/persistence.go
@@ -2,9 +2,11 @@ package persistence
 
 import (
 	"log/slog"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/calamity-m/reap/pkg/errs"
 	"github.com/google/uuid"
 )
 
@@ -38,8 +40,7 @@ type FoodStore interface {
 
 type MemoryFoodStore struct {
 	mux     sync.Mutex
-	tail    int
-	entries map[int]FoodRecordEntry
+	entries map[string]FoodRecordEntry
 	log     *slog.Logger
 }
 
@@ -48,12 +49,11 @@ func (s *MemoryFoodStore) CreateFood(record FoodRecordEntry) error {
 	s.mux.Lock()
 	defer s.mux.Unlock()
 
-	s.entries[s.tail] = record
-	s.tail += 1
+	s.entries[record.Uuid.String()] = record
 
 	if s.log != nil {
 		// Safety debug logging :)
-		s.log.Debug("updated in memory store with a creation", slog.Int("tail", s.tail), slog.Any("entries", s.entries))
+		s.log.Debug("updated in memory store with a creation", slog.Any("entries", s.entries))
 	}
 
 	return nil
@@ -63,23 +63,67 @@ func (s *MemoryFoodStore) CreateFood(record FoodRecordEntry) error {
 // record's uuid. Internal DB primary key is ignored
 // by this call.
 func (s *MemoryFoodStore) GetFood(uuid uuid.UUID) (FoodRecordEntry, error) {
-	panic("not implemented") // TODO: Implement
+	found, ok := s.entries[uuid.String()]
+
+	if !ok {
+		return FoodRecordEntry{}, errs.ErrNotFound
+	}
+
+	return found, nil
 }
 
 // Provided FoodRecordEntry is treated as a filter, allowing
 // the caller to retrieve multiple food records at will.
 func (s *MemoryFoodStore) GetManyFood(filter FoodRecordEntry) ([]FoodRecordEntry, error) {
-	panic("not implemented") // TODO: Implement
+	entries := make([]FoodRecordEntry, 0, 1)
+
+	for _, val := range s.entries {
+		if strings.Contains(val.Description, filter.Description) {
+			entries = append(entries, val)
+		}
+		if strings.Contains(val.Name, filter.Name) {
+			entries = append(entries, val)
+		}
+		if val.UserId == filter.Uuid {
+			entries = append(entries, val)
+		}
+		if val.KJ == filter.KJ {
+			entries = append(entries, val)
+		}
+		if val.Grams == filter.Grams {
+			entries = append(entries, val)
+		}
+		if val.ML == filter.ML {
+			entries = append(entries, val)
+		}
+	}
+
+	return entries, nil
 }
 
 // Update the record in place
 func (s *MemoryFoodStore) UpdateFood(record FoodRecordEntry) error {
-	panic("not implemented") // TODO: Implement
+	s.mux.Lock()
+	defer s.mux.Unlock()
+
+	s.entries[record.Uuid.String()] = record
+
+	if s.log != nil {
+		// Safety debug logging :)
+		s.log.Debug("updated in memory store with update", slog.Any("entries", s.entries))
+	}
+
+	return nil
 }
 
 // Delete matching record
 func (s *MemoryFoodStore) DeleteFood(uuid uuid.UUID) error {
-	panic("not implemented") // TODO: Implement
+	s.mux.Lock()
+	defer s.mux.Unlock()
+
+	delete(s.entries, uuid.String())
+
+	return nil
 }
 
 func NewMemoryFoodStore() *MemoryFoodStore {
